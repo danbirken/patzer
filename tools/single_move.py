0683109c33cd35e1be8b473d1dc9bb73d1c8e541
Dan Birken <birken@gmail.com>
2014-09-23 16:15:36 -0700

    Make best move potentially include pre-made moves

--- tools/single_move.py
+++ tools/single_move.py
@@ -9,8 +9,8 @@ class SingleMoveEngine():
         self.patzer.set_option('hash', 500)
         self.patzer.set_option('ponder', False)
 
-    def make_best_move(self, fen, timeout=None, **kwargs):
+    def make_best_move(self, fen, moves, timeout=None, **kwargs):
         self.patzer.new_game()
-        self.patzer.set_fen_position(fen)
+        self.patzer.set_fen_position(fen, moves=moves)
         best_move = self.patzer.go_and_get_best_move(timeout=timeout, **kwargs)
-        self.patzer.set_fen_position(fen, moves=[best_move.best_move])
+        self.patzer.set_fen_position(fen, moves=moves + [best_move.best_move])
